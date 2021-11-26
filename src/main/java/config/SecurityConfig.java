1d10867b2b9620e24ec1ab7088899d1779d74b85
Leandro <leandro_samorim@hotmail.com>
2021-11-26 18:05:11 -0300

    Initial Commit2

--- src/main/java/config/SecurityConfig.java
+++ src/main/java/config/SecurityConfig.java
@@ -5,17 +5,11 @@
  */
 package config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  *
@@ -33,24 +27,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
            .httpBasic();
         
     }
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
-         auth.inMemoryAuthentication()
-       .passwordEncoder(new BCryptPasswordEncoder())
-           .withUser("technicalsand")
-           .password(passwordEncoder().encode("technicalsand"))
-           .roles("ADMIN")
-       .and()
-           .withUser("reader")
-           .password(passwordEncoder().encode("reader1"))
-           .roles("USER")
-       ;
-    
-    }
-    
-    @Bean
-public PasswordEncoder passwordEncoder() {
-   return new BCryptPasswordEncoder();
-}
     
 }
